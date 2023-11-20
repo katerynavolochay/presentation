a64529964e4777851d69a82ebf64d45009624fd6
katerynavolochay <148555260+katerynavolochay@users.noreply.github.com>
2023-11-20 02:15:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation Deck')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSaBx4udmh8hYvD7r8iQPMGpdrOCYPjKTo09pezMX5dyVNHJ0CBu2n8oEX-7DR-j9hGRvB-EBeI9Yaz/embed?start=false&loop=false&delayms=3000", height=480)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRWvM9nwhwOR6i6p3eL7ID6udtc1oiNpr7XQMwXNJ9eMBriXV9sT4bX3haL4Mjh0nUBedzTue7AAlNM/embed?start=false&loop=false&delayms=3000", height=480)
